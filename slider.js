7ea5162b016b9ed0cb56e173f3e702d3c5f27967
HerlynMae <torresherlynmae@gmail.com>
2025-03-04 07:52:25 +0800

    with slider

---
+++ slider.js
@@ -0,0 +1,13 @@
+var slider = tns({
+  container: ".slider",
+  items: 1,
+  speed: 10,
+  gutter: .1,
+  slideBy: 1,
+  autoplay: true,
+  autoplayPosition: "top",
+  mode: "carousel", 
+  axis: "vertically",
+  prevButton: "false",
+  nextButton: "false",
+});
