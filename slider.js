0ce106eafaacceec57adf71ea806a40690c17f49
saciinol <darrenicolas18@gmail.com>
2025-03-06 10:34:03 +0800

    slider

--- slider.js
+++ slider.js
@@ -1,5 +1,19 @@
-var slider = tns({
+var slider1 = tns({
   container: ".smallSlider",
+  items: 1,
+  speed: 10,
+  gutter: -0.5,
+  slideBy: 1,
+  autoplay: true,
+  autoplayPosition: "top",
+  mode: "carousel",
+  axis: "vertically",
+  prevButton: "false",
+  nextButton: "false",
+});
+
+var slider2 = tns({
+  container: ".bigSlider",
   items: 1,
   speed: 10,
   gutter: 0.1,
